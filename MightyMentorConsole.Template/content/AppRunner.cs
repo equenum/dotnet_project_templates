a45f5961b9ef2772270f8a01d4649f97c06908e1
Egor Quenum <georgequenum@yahoo.com>
2024-03-31 21:49:14 +0400

    Implement MightyMentorConsole.Template starting point

---
+++ MightyMentorConsole.Template/content/AppRunner.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MightyMentorConsole.Template;
+
+internal class AppRunner
+{
+    internal void Run(string[] args)
+    {
+        // application entrypoint is here
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+    }
+}
--- MightyMentorConsole.Template/content/Program.cs
+++ MightyMentorConsole.Template/content/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MightyMentorConsole.Template;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        Configure(host); 
+
+        var appRunner = ActivatorUtilities.CreateInstance<AppRunner>(host.Services);
+        appRunner.Run(args);
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) => 
+        Host.CreateDefaultBuilder(args)
+            .UseConsoleLifetime()
+            .ConfigureServices((context, services) => 
+            {
+                // configure DI services here
+                services.AddTransient<AppRunner>();
+            });
+
+    private static void Configure(IHost host) 
+    {
+        // use IHost to retrieve appsettings.json configs
+        Console.Title = "MightyConsole.Template";
+    }
+}
