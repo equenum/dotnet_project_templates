93fcc3f113ce13a9fd67d263a2d5ca2b42a8ccf3
Egor Quenum <georgequenum@yahoo.com>
2024-03-31 21:17:15 +0400

    Restructure MightyConsole.Template project files

---
+++ MightyConsole.Template/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MightyConsole.Template;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var host = CreateHostBuilder(args).Build();
+        Configure(host); 
+
+        var appRunner = ActivatorUtilities.CreateInstance<AppRunner>(host.Services);
+        appRunner.Run(args);
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) => 
+        Host.CreateDefaultBuilder(args)
+            .UseConsoleLifetime()
+            .ConfigureServices((context, services) => 
+            {
+                // configure DI services here
+                services.AddTransient<AppRunner>();
+            });
+
+    private static void Configure(IHost host) 
+    {
+        // use IHost to retrieve appsettings.json configs
+        Console.Title = "MightyConsole.Template";
+    }
+}
