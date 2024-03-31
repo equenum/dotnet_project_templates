884799c0c5f00f03b095d19db7e8247b20a2d01b
Egor Quenum <georgequenum@yahoo.com>
2024-03-31 18:05:47 +0400

    Add app hosting

--- MightyConsole.Template/src/Program.cs
+++ MightyConsole.Template/src/Program.cs
@@ -1,11 +1,25 @@
 ﻿using System;
+using Microsoft.Extensions.Hosting;
 
 namespace MightyConsole.Template;
 
-class Program
+internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // todo set custom console name
+        Console.Title = "MightyConsole.Template";
+
+        // todo create and run the builder
+        var builder = CreateHostBuilder(args);
     }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) => 
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, services) => 
+            {
+                // Configure DoC services here
+            });
 }
+
+
