74c6e78e6cf7d23ce792f9b5b262b42ab13facab
Egor Quenum <georgequenum@yahoo.com>
2024-04-01 23:29:10 +0400

    Implement initial AppRunner in MightyMentorConsole.Template

--- MightyMentorConsole.Template/content/AppRunner.cs
+++ MightyMentorConsole.Template/content/AppRunner.cs
@@ -1,13 +1,33 @@
 using System;
+using MightyMentorConsole.Template.Utilities;
 
 namespace MightyMentorConsole.Template;
 
 internal class AppRunner
 {
+    // add config option to remove all comments
+
     internal void Run(string[] args)
+    {
+        // application entrypoint is here
+        // Console.WriteLine("Hello, World!");
+        // Console.ReadLine();
+
+        while (true) 
+        {
+            Execute();
+            ColorConsole.WriteSeparationLine();
+        }
+    }
+
+    private static void Execute()
     {
         // application entrypoint is here
         Console.WriteLine("Hello, World!");
         Console.ReadLine();
+
+        // with utils
+        ColorConsole.WriteLine("Hello, World!");
+        ColorConsole.ReadLine();
     }
 }
