1b8c06e497156e63b1a3c4b45ae65bc1d8e28686
Egor Quenum <georgequenum@yahoo.com>
2024-04-03 00:32:25 +0400

    Update application entrypoint in AppRunner

--- MightyMentorConsole.Template/content/MightyMentorConsole.Template/AppRunner.cs
+++ MightyMentorConsole.Template/content/MightyMentorConsole.Template/AppRunner.cs
@@ -7,18 +7,6 @@ internal class AppRunner
     internal void Run(string[] args)
     {
         // application entrypoint is here
-        // ColorConsole.WriteLine("Hello, World!");
-
-        while (true) 
-        {
-            Execute();
-            ColorConsole.WriteSeparationLine();
-        }
-    }
-
-    private static void Execute()
-    {
-        // application entrypoint is here
-        // ColorConsole.WriteLine("Hello, World!");
+        ColorConsole.WriteLine("Hello, World!");
     }
 }
