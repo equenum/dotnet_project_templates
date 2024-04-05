42b03d550f5e4e9ee6d1a5406bd0d840f7ec316e
Egor Quenum <georgequenum@yahoo.com>
2024-04-05 23:17:49 +0400

    Add IncludeUtils template symbol handling

--- MightyMentorConsole.Template/content/MightyMentorConsole.Template/AppRunner.cs
+++ MightyMentorConsole.Template/content/MightyMentorConsole.Template/AppRunner.cs
@@ -1,4 +1,8 @@
+#if IncludeUtils
 using MightyMentorConsole.Template.Utilities;
+#else
+using System;
+#endif
 
 namespace MightyMentorConsole.Template;
 
@@ -7,6 +11,10 @@ internal class AppRunner
     internal void Run(string[] args)
     {
         // application entrypoint is here
+        #if IncludeUtils
         ColorConsole.WriteLine("Hello, World!");
+        #else
+        Console.WriteLine("Hello, World!");
+        #endif
     }
 }
